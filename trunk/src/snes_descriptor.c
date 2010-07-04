448b4a536ca73c0da203b4677fcfc60f0d430195
tim.helloworld <tim.helloworld@a2ac766f-7c79-7993-8e68-4bdd792dbcde>
2010-07-04 17:16:22 +0000

    finishing rep

--- trunk/src/snes_descriptor.c
+++ trunk/src/snes_descriptor.c
@@ -20,33 +20,31 @@
 
 
 static const char snes_usbHidReportDescriptor[] PROGMEM = {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x05,                    // USAGE (Game Pad)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //     LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x02,                    //   REPORT_COUNT (2)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0xc0,                          // END_COLLECTION
-    0x05, 0x09,                    // USAGE_PAGE (Button)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-    0x29, 0x0A,                    //   USAGE_MAXIMUM (Button 8)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    // REPORT_SIZE (1)
-    0x95, 0x0A,                    // REPORT_COUNT (19)
-    0x81, 0x02,                    // INPUT (Data,Var,Abs)
-	
-    0x75, 0x01,                    // REPORT_SIZE (1)
-    0x95, 0x0E,                    // REPORT_COUNT (19)
-    0x81, 0x01,                    // INPUT (Data,Var,Abs)
-	
-    0xc0                           // END_COLLECTION
+	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+	0x09, 0x05,                    // USAGE (Game Pad)
+	0xa1, 0x01,                    // COLLECTION (Application)
+	0x09, 0x01,                    //   USAGE (Pointer)
+	0xa1, 0x00,                    //   COLLECTION (Physical)
+	0x09, 0x30,                    //     USAGE (X)
+	0x09, 0x31,                    //     USAGE (Y)
+	0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+	0x26, 0xff, 0x00,              //     LOGICAL_MAXIMUM (255)
+	0x75, 0x08,                    //     REPORT_SIZE (8)
+	0x95, 0x02,                    //     REPORT_COUNT (2)
+	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+	0xc0,                          //   END_COLLECTION
+	0x05, 0x09,                    //   USAGE_PAGE (Button)
+	0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+	0x29, 0x0a,                    //   USAGE_MAXIMUM (Button 10)
+	0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+	0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,                    //   REPORT_SIZE (1)
+	0x95, 0x0a,                    //   REPORT_COUNT (10)
+	0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+	0x75, 0x01,                    //   REPORT_SIZE (1)
+	0x95, 0x06,                    //   REPORT_COUNT (6)
+	0x81, 0x01,                    //   INPUT (Cnst,Ary,Abs)
+	0xc0                           //   END_COLLECTION
 };
 
 
