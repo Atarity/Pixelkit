548f1257ee587095102310a4310a838ef492926e
atarity <atarity@a2ac766f-7c79-7993-8e68-4bdd792dbcde>
2010-08-15 13:29:51 +0000

    [issue-4] ps3 support added for sega gamepad

--- trunk/src/sega_descriptor.c
+++ trunk/src/sega_descriptor.c
@@ -1,5 +1,6 @@
 /* Nes/Snes/Genesis/SMS/Atari to USB
  * Copyright (C) 2006-2007 Raphaël Assénat
+ * modified by Timur Sattarov (tim.helloworld@gmail.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,28 +21,31 @@
 
 
 static const char sega_usbHidReportDescriptor[] PROGMEM = {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x05,                    // USAGE (Game Pad)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //     LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x02,                    //   REPORT_COUNT (2)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0xc0,                          // END_COLLECTION
-    0x05, 0x09,                    // USAGE_PAGE (Button)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-    0x29, 0x08,                    //   USAGE_MAXIMUM (Button 8)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    // REPORT_SIZE (1)
-    0x95, 0x08,                    // REPORT_COUNT (8)
-    0x81, 0x02,                    // INPUT (Data,Var,Abs)
-    0xc0                           // END_COLLECTION
+	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+	0x09, 0x05,                    // USAGE (Game Pad)
+	0xa1, 0x01,                    // COLLECTION (Application)
+	0x09, 0x01,                    //   USAGE (Pointer)
+	0xa1, 0x00,                    //   COLLECTION (Physical)
+	0x09, 0x30,                    //     USAGE (X)
+	0x09, 0x31,                    //     USAGE (Y)
+	0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+	0x26, 0xff, 0x00,              //     LOGICAL_MAXIMUM (255)
+	0x75, 0x08,                    //     REPORT_SIZE (8)
+	0x95, 0x02,                    //     REPORT_COUNT (2)
+	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+	0xc0,                          //   END_COLLECTION
+	0x05, 0x09,                    //   USAGE_PAGE (Button)
+	0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+	0x29, 0x0a,                    //   USAGE_MAXIMUM (Button 10)
+	0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+	0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,                    //   REPORT_SIZE (1)
+	0x95, 0x0a,                    //   REPORT_COUNT (10)
+	0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+	0x75, 0x01,                    //   REPORT_SIZE (1)
+	0x95, 0x06,                    //   REPORT_COUNT (6)
+	0x81, 0x01,                    //   INPUT (Cnst,Ary,Abs)
+	0xc0                           //   END_COLLECTION
 };
 
 
